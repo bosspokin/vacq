9e2cbf3eb2b60dd961fe5edc1c76dc11d1f8288f
Pokin Chaitanasakul <pokin67890@gmail.com>
2023-01-30 11:23:05 +0700

    Connect with MongoDB Atlas

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-// Route files
-const hospitals = require('./routes/hospital')
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({path: './config/config.env'});
 
+// Connect to database
+connectDB();
+
+// Route files
+const hospitals = require('./routes/hospital')
+
+
 const app = express();
+
+// add body parser middleware
+app.use(express.json());
 
 // Mount routers
 app.use('/api/v1/hospitals', hospitals)
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server & exit process
+    server.close(() => process.exit(1));
+});
