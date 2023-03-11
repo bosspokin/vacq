13083cc00a249bda2296a459109f24d3f5a1c56f
Pokin Chaitanasakul <pokin67890@gmail.com>
2023-03-11 20:24:02 +0700

    implement pagination for get hospitals

--- controllers/hospital.js
+++ controllers/hospital.js
@@ -4,12 +4,71 @@ const Hospital = require("../models/Hospital");
 // @route GET /api/v1/hospitals
 // @access Public
 exports.getHospitals = async (req, res, next) => {
-  try {
-    const hospitals = await Hospital.find();
+  let query;
 
-    res
-      .status(200)
-      .json({ success: true, count: hospitals.length, data: hospitals });
+  // Copy req.query
+  const reqQuery = { ...req.query };
+
+  // Fields to exclude
+  const removeFields = ["select", "sort", "page", "limit"];
+  removeFields.forEach((param) => delete reqQuery[param]);
+  console.log(reqQuery);
+
+  // Create query string
+  let queryStr = JSON.stringify(reqQuery);
+
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+  query = Hospital.find(JSON.parse(queryStr));
+
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  try {
+    const total = await Hospital.countDocuments();
+    query.skip(startIndex).limit(limit);
+
+    // Execute query
+    const hospitals = await query;
+
+    // Pagination result
+    const pagination = {};
+
+    console.log(`endIndex: ${endIndex}, total: ${total}`);
+    if (endIndex < total) {
+      pagination.next = {
+        page: page + 1,
+        limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      pagination.last = {
+        page: page - 1,
+        limit,
+      };
+    }
+    res.status(200).json({
+      success: true,
+      count: hospitals.length,
+      pagination,
+      data: hospitals,
+    });
   } catch (err) {
     res.status(500).json({ success: false });
   }
