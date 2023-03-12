a68bf0d852f5544bcd1125aaf84fd8ac30cc7ae5
Pokin Chaitanasakul <pokin67890@gmail.com>
2023-03-12 15:16:01 +0700

    implement getAppointment

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -3,7 +3,6 @@ const Appointment = require("../models/Appointment");
 // @desc Get all appointments
 // @route /api/v1/appointments
 // @access Public
-
 exports.getAppointments = async (req, res, next) => {
   let query;
 
@@ -31,6 +30,35 @@ exports.getAppointments = async (req, res, next) => {
       data: appointments,
     });
   } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Cannot find Appointment" });
+  }
+};
+
+// @desc Get single appointment
+// @route GET /api/v1/appointments/:id
+// @access Public
+exports.getAppointment = async (req, res, next) => {
+  try {
+    const appointment = await Appointment.findById(req.params.id).populate({
+      path: "hospital",
+      select: " name description tel",
+    });
+
+    if (!appointment) {
+      return res.status(404).json({
+        success: false,
+        message: `No appointment with the id of ${req.paramas.id}`,
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: appointment,
+    });
+  } catch (err) {
     console.log(error);
     return res
       .status(500)
--- routes/appointments.js
+++ routes/appointments.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
-const { getAppointments } = require("../controllers/appointments");
+const {
+  getAppointments,
+  getAppointment,
+} = require("../controllers/appointments");
 const { protect } = require("../middleware/auth");
 
 const router = express.Router({ mergeParams: true });
 
 router.route("/").get(protect, getAppointments);
+router.route("/:id").get(protect, getAppointment);
 
 module.exports = router;
